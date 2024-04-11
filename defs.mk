0a1256638fd031c9e4234d0d1aac75dfc579155f
ftello27 <ftello@clemson.edu>
2024-04-11 02:57:22 -0400

    defs.mk comments

--- defs.mk
+++ defs.mk
@@ -1,7 +1,7 @@
-# [Code Description]
+# Makefile fragments for Project 01: Strands, contains .c and header files
 # Author: Fernando Tello
 # CPSC1070 - Spring 2024
 # Project 01: Strands
 
-SOURCES = strand.c game.c
-HEADERS = strand.h game.h
+SOURCES = strand.c game.c # .c files
+HEADERS = strand.h game.h # header files
