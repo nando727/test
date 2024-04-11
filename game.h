bc29271c73d359e10f3eb5276a0c4c0c14569473
ftello27 <ftello@clemson.edu>
2024-04-11 03:20:35 -0400

    game.h comments

--- game.h
+++ game.h
@@ -1,4 +1,4 @@
-/* [Code Description]
+/* Definitions & function prototypes for game.c
 Author: Fernando Tello
 CPSC1070 - Spring 2024
 Project 01: Strands */
@@ -11,12 +11,12 @@ Project 01: Strands */
 
 #include "strand.h"
 
-#define DICT_PATH "/usr/share/dict/american-english"
+#define DICT_PATH "/usr/share/dict/american-english" // default dictionary file path
 
-// REQUIRED FUNCTIONS
-FILE * open_data_file(int argc, char** argv);
-FILE * open_dict_file(int argc, char** argv);
-void print_instructions(strand_t * strand);
-void play_game(strand_t * strand);
+// function prototypes
+FILE * open_data_file(int argc, char** argv); // opens & returns data file if provided as second argument, user input otherwise
+FILE * open_dict_file(int argc, char** argv); // opens & returns dictionary file if provided as third argument, default file otherwise
+void print_instructions(strand_t * strand); // prints game instructions
+void play_game(strand_t * strand); // main game loop, handles correct, incorrect, and repeated guesses, allows user to quit game
 
 #endif
